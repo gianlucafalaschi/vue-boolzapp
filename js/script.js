e2876c7add9a1f8f68b640c52de77a026d7521b4
gianlucafalaschi <gian100gian@outlook.it>
2024-04-04 19:08:59 +0200

    addNewMessage function

--- js/script.js
+++ js/script.js
@@ -84,14 +84,38 @@ createApp({
               },
             ], 
             activeUser: 0,
+            
+            newMessageText: '',
         };
     }, 
     methods: {
       showMessages(index){
         this.activeUser = index;
         console.log(this.activeUser);
+      },
+      addNewMessage(textToadd){
+        alert(textToadd);
       }
     }
 }).mount('#app');
 
 
+// Milestone 1
+//- Replica della grafica con la possibilità di avere messaggi scritti dall’utente (verdi) 
+//e dall’interlocutore (bianco) assegnando due classi CSS diverse
+
+//- Visualizzazione dinamica della lista contatti: tramite la direttiva v-for, visualizzare 
+//nome e immagine di ogni contatto
+
+//Milestone 2
+//- Visualizzazione dinamica dei messaggi: tramite la direttiva v-for, visualizzare tutti i 
+//messaggi relativi al contatto attivo all’interno del pannello della conversazione
+
+//- Click sul contatto mostra la conversazione del contatto cliccato
+
+// Milestone 3
+//- Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e digitando 
+// “enter” il testo viene aggiunto al thread sopra, come messaggio verde
+
+//- Risposta dall’interlocutore: ad ogni inserimento di un messaggio, l’utente 
+//riceverà un “ok” come risposta, che apparirà dopo 1 secondo.

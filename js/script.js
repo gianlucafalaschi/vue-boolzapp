1e2f09119fcad827ed4d8ab072e96d8a69102297
gianlucafalaschi <gian100gian@outlook.it>
2024-04-05 17:56:53 +0200

    milestone 4 start

--- js/script.js
+++ js/script.js
@@ -91,6 +91,8 @@ createApp({
 
             currentDate: '',
 
+            searchInput: '',
+
         };
     }, 
     methods: {
@@ -133,6 +135,9 @@ createApp({
         }, 3000);
          
       },
+      filterUser(){
+
+      },
 
     }
 }).mount('#app');
@@ -157,3 +162,9 @@ createApp({
 
 //- Risposta dall’interlocutore: ad ogni inserimento di un messaggio, l’utente 
 //riceverà un “ok” come risposta, che apparirà dopo 1 secondo.
+
+// Milestone 4
+// - Ricerca utenti: scrivendo qualcosa nell’input a sinistra, vengono 
+//visualizzati solo i contatti il cui nome contiene le lettere inserite (es, Marco, Matteo Martina -> 
+// Scrivo “mar” rimangono solo Marco e Martina)
+
--- js/test.js
+++ js/test.js
@@ -1,3 +1,5 @@
 /* const dt = luxon.DateTime;
 const now = dt.now().setLocale('it').toLocaleString(dt.DATETIME_SHORT_WITH_SECONDS);
 console.log(now); */
+
+
